dc27fe0f927bf059fd51d950652f54d68a2423a3
L17 <a.alabdullatif.17@gmail.com>
2025-02-07 05:32:58 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,36 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = "blackjack-v1";
+const assets = [
+    "index.html",
+    "style.css",
+    "script.js",
+    "manifest.json"
+];
+
+// Install service worker & cache assets
+self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open("blackjack-v1").then((cache) => {
-            return cache.addAll([
-                "index.html",
-                "styles.css",
-                "script.js",
-                "manifest.json",
-                "icon-192.png",
-                "icon-512.png"
-            ]);
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assets);
         })
     );
 });
 
-self.addEventListener("fetch", (event) => {
+// Serve cached files when offline
+self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
+        })
+    );
+});
+
+// Update cache when new version is available
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
         })
     );
 });
