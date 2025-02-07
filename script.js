8d18c923461e77ec66d4fbce36ef459d201f9027
L17 <a.alabdullatif.17@gmail.com>
2025-02-07 04:55:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let balance = 1000;
 let betAmount = 0;
+let playerHand = [];
+let dealerHand = [];
 let deck = [];
 
 document.getElementById("start-game").addEventListener("click", () => {
@@ -19,6 +21,8 @@ document.querySelectorAll(".chip").forEach(chip => {
 });
 
 document.getElementById("deal-button").addEventListener("click", startGame);
+document.getElementById("hit-button").addEventListener("click", playerHit);
+document.getElementById("stand-button").addEventListener("click", dealerPlay);
 document.getElementById("restart-button").addEventListener("click", restartGame);
 
 function startGame() {
@@ -29,7 +33,15 @@ function startGame() {
 
     deck = createDeck();
     shuffleDeck(deck);
+
+    playerHand = [drawCard(), drawCard()];
+    dealerHand = [drawCard(), drawCard()];
+
+    updateGameUI();
+
     document.getElementById("deal-button").disabled = true;
+    document.getElementById("hit-button").disabled = false;
+    document.getElementById("stand-button").disabled = false;
 }
 
 function createDeck() {
@@ -51,15 +63,47 @@ function shuffleDeck(deck) {
     }
 }
 
+function drawCard() {
+    return deck.pop();
+}
+
+function playerHit() {
+    playerHand.push(drawCard());
+    updateGameUI();
+
+    if (calculateScore(playerHand) > 21) {
+        endGame("Bust! You lose.");
+    }
+}
+
+function dealerPlay() {
+    while (calculateScore(dealerHand) < 17) {
+        dealerHand.push(drawCard());
+    }
+    updateGameUI();
+    determineWinner();
+}
+
+function determineWinner() {
+    let playerScore = calculateScore(playerHand);
+    let dealerScore = calculateScore(dealerHand);
+
+    if (dealerScore > 21 || playerScore > dealerScore) {
+        endGame("You win!");
+    } else {
+        endGame("Dealer wins!");
+    }
+}
+
+function endGame(message) {
+    document.getElementById("message").textContent = message;
+    document.getElementById("hit-button").disabled = true;
+    document.getElementById("stand-button").disabled = true;
+}
+
 function restartGame() {
     balance = 1000;
     betAmount = 0;
     updateBetUI();
 }
-
-function updateBetUI() {
-    document.getElementById("bet-amount").textContent = betAmount;
-    document.getElementById("balance").textContent = balance;
-}
-
 
